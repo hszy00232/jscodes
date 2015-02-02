9eb91ee87ea0ba040102413239eeb0125c42d592
郭培培 <guopeipei@360.cn>
2015-02-02 16:33:27 +0800

    添加语法高亮使用highlight.js 添加自动生成目录使用marked-doc

--- App/Lib/Controller/Home/IndexController.js
+++ App/Lib/Controller/Home/IndexController.js
@@ -5,11 +5,12 @@
 var fs = require("fs");
 var path = require("path");
 var marked = require("marked");
+var markToc = require("marked-toc");
+var uslug = require('uslug');
 // 当前项目的根目录
 var root = path.resolve(APP_PATH, '..');
 
-var hreg = /(<h2[^<>]*>(.*?)<\/h2>)/g;
-var titReg = /title:[\s]?(\S*)/g
+var hreg = /<h(\d)[^<>]*>(.*?)<\/h\1>/g;
 
 module.exports = Controller("Home/BaseController", function(){
 	"use strict";
@@ -23,14 +24,16 @@ module.exports = Controller("Home/BaseController", function(){
 				layoutContent = content;
 				return self.fetch(viewFile);
 			}).then(function(content){
+				var toc = marked(markToc(content));
 				content = marked(content);
-				content = content.replace(titReg,function(a,b){
-					layoutContent = layoutContent.replace('<!--!doc-title-->',b);
+				
+				content = content.replace(hreg,function(a,b,c){
+					var id = uslug(c, {allowedChars: "-"});
+					return '<div class="chapter"><h'+b+' id="'+id+'">'+c+'</h'+b+'></div>';
 				});
-				content = content.replace(hreg,function(a){
-					return '<div class="chapter">'+a+'</div>';
-				});
+
 				layoutContent = layoutContent.replace('<!--!doc-content-->', content);
+				layoutContent = layoutContent.replace('<!--!toc-content-->', toc);
 				return self.end(layoutContent);
 			});
 		}
